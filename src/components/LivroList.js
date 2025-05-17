74395899c19bd7a5756fb623757dc64b396a7261
Rivanildo Pires <rivanildopires1@gmail.com>
2025-05-17 01:16:00 -0300

    pequeno teste

--- src/components/LivroList.js
+++ src/components/LivroList.js
@@ -1,4 +1,11 @@
 import React, { useState, useEffect } from "react";
+import "./ListarLivros.css";
+import logo from "../assets/logo.png";
+import livro from "../assets/livro.png";
+import usuario from "../assets/usuario.png";
+import sair from "../assets/sair.png";
+import java from "../assets/java.png";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 const api = axios.create({
@@ -53,37 +60,108 @@ const LivroList = () => {
   }
 
   return (
-    <div className="livro-list">
-      <h2>Lista de Livros</h2>
+    <div>
+      <header>
+        <nav className="nav-bar">
+          <div className="navbar-inner">
+            <div className="navbar-top">
+              <Link to={"/"}>
+                <img src={logo} alt="logo-biblioteca" />
+              </Link>
 
-      {livros.length === 0 ? (
-        <p>Nenhum livro cadastrado.</p>
-      ) : (
-        <div className="table-responsive">
-          <table className="livro-table">
-            <thead>
+              <form className="form">
+                <input type="text" placeholder="Buscar..." />
+              </form>
+              <ul className="list">
+                <li>
+                  <img className="livro-img" src={livro} alt="livrinho" />
+                  <a href="#">
+                    Livros <br />
+                    Reservado
+                  </a>
+                </li>
+                <li>
+                  <img className="usuario-img" src={usuario} alt="usuario" />{" "}
+                  <a href="#">
+                    Meu <br />
+                    Perfil
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <img className="sair-img" src={sair} alt="" />
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <hr />
+            <table className="table">
               <tr>
-                <th>Título</th>
-                <th>Autor</th>
-                <th>Área</th>
-                <th>Editora</th>
-                <th>Ano Publicado</th>
+                <Link to={"/livros"}>
+                  <th>Livros</th>
+                </Link>
+                <th>Artigos</th>
+                <th>TCC</th>
+                <th>Material Acadêmico</th>
               </tr>
-            </thead>
-            <tbody>
-              {livros.map((livro) => (
-                <tr key={livro.uuid || livro.id}>
-                  <td>{livro.titulo}</td>
-                  <td>{livro.autor}</td>
-                  <td>{livro.area}</td>
-                  <td>{livro.editora}</td>
-                  <td>{livro.anoPublicado}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+            </table>
+          </div>
+        </nav>
+      </header>
+
+      <div className="container">
+        <aside className="categories">
+          <h4>Livros</h4>
+          <div className="separator">
+            <div className="line"></div>
+          </div>
+          <ul>
+            <li>Ciências da Computação</li>
+            <li>Direito</li>
+            <li>Educação Fisica</li>
+            <li>Marketing</li>
+            <li>Matemática</li>
+          </ul>
+        </aside>
+        <div className="container-main">
+          <main className="main-content">
+            <h2>Lista de Livros</h2>
+
+            {livros.length === 0 ? (
+              <p>Nenhum livro cadastrado.</p>
+            ) : (
+              <section className="section-livros">
+                {livros.map((livro) => (
+                  <div className="livro" key={livro.uuid || livro.id}>
+                    <img
+                      src={livro.imagem || java}
+                      alt={`Capa do livro ${livro.titulo}`}
+                    />
+                    <h5>{livro.titulo}</h5>
+                    <p>
+                      <strong>Autor:</strong> {livro.autor}
+                    </p>
+                    <p>
+                      <strong>Área:</strong> {livro.area}
+                    </p>
+                    <p>
+                      <strong>Editora:</strong> {livro.editora}
+                    </p>
+                    <p>
+                      <strong>Ano:</strong> {livro.anoPublicado}
+                    </p>
+                  </div>
+                ))}
+              </section>
+            )}
+          </main>
         </div>
-      )}
+      </div>
+      <footer>
+        <h3>
+          Faculdade Católica da Paraíba. © 2025 - Todos os direitos reservados.
+        </h3>
+      </footer>
     </div>
   );
 };
