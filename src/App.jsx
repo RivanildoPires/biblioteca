37ea08e6de474158d4fc706323d7e2c39adac274
Rivanildo Pires <rivanildopires1@gmail.com>
2025-05-27 22:43:43 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<TelaInicial />} />
         <Route path="/livros" element={<ListarLivros />} />
-        <Route path="/livro" element={<Livro />} />
+        <Route path="/livro/:id" element={<Livro />} />
         <Route path="/tcc" element={<ListarTCC />} />
         <Route path="/material" element={<Material />} />
       </Routes>
