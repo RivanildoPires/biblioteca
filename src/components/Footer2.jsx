98a3c372e8c8847032fb2c927c3660581acd73b6
Rivanildo Pires <rivanildopires1@gmail.com>
2025-06-03 15:19:40 -0300

    ajustes finais

--- src/components/Footer2.jsx
+++ src/components/Footer2.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div>
-      <footer>
+      <footer className="footer">
         <h3>
           Faculdade Católica da Paraíba. © 2025 - Todos os direitos reservados.
         </h3>
